ec64890162600ad61bd6dbf74a2bd1cbd6409cdb
danielntemo <ntemo.daniel@seznam.cz>
2025-11-22 23:26:22 +0100

    coomit

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.views.generic import *
 from viewer.models import Information
 
@@ -6,3 +7,6 @@ class InformationView(ListView):
     model = Information
     context_object_name = 'information'
     template_name = 'information.html'
+
+def home(request):
+    return render(request,'base.html')
